e716765ce570af40d26313b1501eb968d6c8d517
Batima <wendellmachado.nave@gmail.com>
2015-04-12 13:24:53 -0300

    Primeiro Teste
    Transição de cenas iniciais, Criação de servidor.

---
+++ Oficina3/Assets/Script/EventHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	public void ChangeScene(string scene)
+	{
+		Application.LoadLevel (scene);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Oficina3/Assets/Script/NetworkManagerLobby.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManagerLobby : MonoBehaviour 
+{
+	private const string GameName = "Rio450Graus";
+	private const string RoomName = "RoomName";
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	public void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(GameName, RoomName);
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
